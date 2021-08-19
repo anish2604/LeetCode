e4f52a67464ea0211020f12b7770e2e858e9828f
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2021-08-19 21:48:46 +0530

    Add files via upload

---
+++ Two Sum.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target)
+    {
+        HashMap<Integer, Integer> comp = new HashMap<Integer, Integer>();
+            for (int i = 0; i < nums.length; i++)
+            {
+                if (comp.containsKey(target-nums[i]))
+                {
+                    return new int[] {comp.get(target-nums[i]), i};
+                }
+                else
+                {
+                    comp.put(nums[i], i);
+                }
+            }
+            return null;
+    }
+}
