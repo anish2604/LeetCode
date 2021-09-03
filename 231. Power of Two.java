cfdef370eb5a6bfa056c8a11d0c6c19e9f027329
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2021-09-03 22:17:15 +0530

    Create 231. Power of Two.java

---
+++ 231. Power of Two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        while(n!=0){
+            if(n%2 == 0){
+                n/=2; 
+            }
+            else return false;
+            if(n==1) return true;
+        }
+        return false;
+    }
+}
