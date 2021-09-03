c244d2ee64417df1786c2017d44bc8ea890fd431
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2021-09-03 22:18:13 +0530

    Create 326. Power of Three.java

---
+++ 326. Power of Three.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1) return true;
+        while(n!=0){
+            if(n%3 == 0){
+                n/=3; 
+            }
+            else return false;
+            if(n==1) return true;
+        }
+        return false;
+    }
+}
