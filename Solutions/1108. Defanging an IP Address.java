1108dcddfaca4f6b4ab4499ed88fbd5a00d9dba9
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2021-09-26 00:02:14 +0530

    Update 1108. Defanging an IP Address.java

--- Solutions/1108. Defanging an IP Address.java
+++ Solutions/1108. Defanging an IP Address.java
@@ -1,12 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder sb = new StringBuilder();
-    
-        for(char a : address.toCharArray()){
-            if(a == '.') sb.append("[.]");
-            else sb.append(a);
-        }
-
-        return sb.toString();
+        String res = address.replace(".","[.]");
+        return res;
     }
 }
