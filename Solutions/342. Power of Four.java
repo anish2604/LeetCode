eefed9a244542e11f6aadfa92daa4c1e74ce9f09
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2022-04-07 08:22:09 +0530

    Update 342. Power of Four.java

--- Solutions/342. Power of Four.java
+++ Solutions/342. Power of Four.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-       if(n==1) return true;
-        while(n!=0){
-            if(n%4 == 0){
-                n/=4; 
-            }
-            else return false;
-            if(n==1) return true;
-        }
-        return false; 
+        if(n == 1)
+            return true;
+        if(n <= 0 || n%4 != 0)
+            return false;
+        
+        // Without using Recursion
+        double k = Math.log(n)/Math.log(4);
+        return Math.floor(k) == k;
+        
+        // Using Recursion
+        // return isPowerOfFour(n>>2);
     }
 }
