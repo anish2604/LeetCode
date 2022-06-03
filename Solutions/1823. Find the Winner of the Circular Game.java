7d2967e227394212cdedc2dded7fd2134b3ade95
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2022-06-03 23:27:07 +0530

    Create 1823. Find the Winner of the Circular Game.java

---
+++ Solutions/1823. Find the Winner of the Circular Game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return ans(n, k) + 1;
+    }
+    
+    public int ans(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        return (ans(n-1, k) + k) % n;
+    }
+}
